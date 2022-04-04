ceaf72ba32bb7d01e329e7db6aa8fe26bd7ee212
ivanaolorteguimoreno <iolortegui@proveedor.intercorp.com.pe>
2022-04-03 21:54:58 -0400

    Implement rout 🙌

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -10,6 +11,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphicOneComponent } from './pages/graphic-one/graphic-one.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,10 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     DashboardComponent,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent,
+    ProgressComponent,
+    GraphicOneComponent
   ],
   imports: [
     BrowserModule,
